d7b3ae1d4f6e14aa436154c758d96f603ee6fdda
DacDo <mr.dacdo@gmail.com>
2021-06-07 03:59:07 +1000

    updated

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+provider "aws" {
+  region = "us-west-2"
+}
+resource "aws_vpc" "dd_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.dd_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.dd_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
 
 terraform {
   backend "remote" {
@@ -29,7 +45,7 @@ resource "aws_instance" "dev-vm1" {
   ami = "ami-000d6375f955d3d80"
   subnet_id = aws_subnet.subnet1.id
   instance_type = lookup(var.inst_type, var.env)
-  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
   tags = {
     Name = "DacDo"
   }
@@ -38,7 +54,7 @@ resource "aws_instance" "dev-vm2" {
   ami = "ami-018e347dfe0c44534"
   subnet_id = aws_subnet.subnet2.id
   instance_type = lookup(var.inst_type, var.env)
-  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
   tags = {
     Name = "ThuyVu"
   }
